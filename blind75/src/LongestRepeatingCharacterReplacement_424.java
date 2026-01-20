78416cc3178505809aa4d165706bf1d0900d33fc
chinmay768 <chinmaysahani07@gmail.com>
2026-01-20 23:34:29 +0530

    Character Replacement

---
+++ blind75/src/LongestRepeatingCharacterReplacement_424.java
@@ -0,0 +1,58 @@
+public class LongestRepeatingCharacterReplacement_424 {
+
+    public int characterReplacement(String s, int k) {
+        int[] hash = new int[26];
+        int left = 0, right = 0;
+        int maxFreq = 0;
+        int maxLen = 0;
+        while (right < s.length()) {
+            hash[s.charAt(right) - 'A']++;
+
+            maxFreq = Math.max(maxFreq, hash[s.charAt(right) - 'A']);
+
+            while ((right - left + 1 ) - maxFreq > k){
+                hash[s.charAt(left) - 'A']--;
+                left++;
+
+                for (int i = 0; i < hash.length; i++) maxFreq = Math.max(maxFreq, hash[i]);
+            }
+
+            maxLen = Math.max(maxLen, right - left + 1);
+            right++;
+        }
+
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+/*
+You are given a string s and an integer k. You can choose any character of the string and change it to any other uppercase English character. You can perform this operation at most k times.
+
+Return the length of the longest substring containing the same letter you can get after performing the above operations.
+
+
+
+Example 1:
+
+Input: s = "ABAB", k = 2
+Output: 4
+Explanation: Replace the two 'A's with two 'B's or vice versa.
+Example 2:
+
+Input: s = "AABABBA", k = 1
+Output: 4
+Explanation: Replace the one 'A' in the middle with 'B' and form "AABBBBA".
+The substring "BBBB" has the longest repeating letters, which is 4.
+There may exists other ways to achieve this answer too.
+
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of only uppercase English letters.
+0 <= k <= s.length
+ */
