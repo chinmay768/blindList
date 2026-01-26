f02346b6598e54d65a73374a9ef9e241ad69de11
chinmay768 <chinmaysahani07@gmail.com>
2026-01-26 22:47:12 +0530

    Detect Cycle in LL

---
+++ blind75/src/LinkedListCycle_141.java
@@ -0,0 +1,85 @@
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class LinkedListCycle_141 {
+    private class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+    }
+
+    //TC: O(n)  SC: O(n)
+    public boolean hasCycleNaive(ListNode head) {
+        Set<ListNode> st = new HashSet<>();
+        ListNode temp = head;
+        while (temp != null) {
+            if(st.contains(temp)) return true;
+
+            st.add(temp);
+            temp = temp.next;
+        }
+
+        return false;
+    }
+
+    // TC: O(n)  SC: O(1)
+    public boolean hasCycleOptimal(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast) return true;
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+/*
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer.
+Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false.
+
+
+
+Example 1:
+
+
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+Example 2:
+
+
+Input: head = [1,2], pos = 0
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+Example 3:
+
+
+Input: head = [1], pos = -1
+Output: false
+Explanation: There is no cycle in the linked list.
+
+
+Constraints:
+
+The number of the nodes in the list is in the range [0, 104].
+-105 <= Node.val <= 105
+pos is -1 or a valid index in the linked-list.
+ */
