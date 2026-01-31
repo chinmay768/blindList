79baf420facf3587e1588f8e9182a78ae42b1995
chinmay768 <chinmaysahani07@gmail.com>
2026-01-31 23:22:04 +0530

    Max Prod Subarray

--- blind75/src/MaximumProductSubarray_152.java
+++ blind75/src/MaximumProductSubarray_152.java
@@ -1,6 +1,21 @@
 public class MaximumProductSubarray_152 {
     public int maxProduct(int[] nums) {
+        int preProduct = 1;
+        int postProduct = 1;
+        int maxProduct = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++){
+            if(preProduct == 0) preProduct = 1;
 
+            if(postProduct == 0) postProduct = 1;
+
+            // Keep it below if conditions so as to update maxProduct in case of 0 and all -ve ([-2,0,-1])
+            preProduct = preProduct * nums[i];
+            postProduct = postProduct * nums[nums.length - 1 - i];
+
+            maxProduct = Math.max(maxProduct, Math.max(preProduct, postProduct));
+        }
+
+        return maxProduct;
     }
 
 
