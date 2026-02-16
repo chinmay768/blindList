57f54ee0a2318d7e81a9c8e1cf629c2d23f1bb3f
chinmay768 <chinmaysahani07@gmail.com>
2026-02-16 21:06:31 +0530

    Longest Consecutive Suquence

--- blind75/src/LongestConsecutiveSequence_128.java
+++ blind75/src/LongestConsecutiveSequence_128.java
@@ -29,6 +29,7 @@ public class LongestConsecutiveSequence_128 {
     }
 
     public int longestConsecutiveOptimal(int[] nums) {
+        if(nums.length == 0) return 0;
         Set<Integer> set = new HashSet<>();
 
         for (int i = 0; i < nums.length; i++) {
