29f07f6f8f3dc4999f6c32fae73a057efaec7c33
chinmay768 <chinmaysahani07@gmail.com>
2026-02-17 18:12:40 +0530

    Product Except Self

--- blind75/src/ProductOfArrayExceptSelf_238.java
+++ blind75/src/ProductOfArrayExceptSelf_238.java
@@ -23,6 +23,24 @@ public class ProductOfArrayExceptSelf_238 {
         return res;
     }
 
+    // TC: O(n)  SC: O(1)
+    public int[] productExceptSelfOptimal(int[] nums) {
+        int[] res = new int[nums.length];
+
+        res[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            res[i] = res[i - 1] * nums[i - 1];
+        }
+
+        int suffix = 1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            suffix *= nums[i + 1];
+            res[i] *= suffix;
+        }
+
+        return res;
+    }
+
     public static void main(String[] args) {
 
     }
