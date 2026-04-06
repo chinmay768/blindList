a8691841af919565507f51f751891569748f876f
chinmay768 <chinmaysahani07@gmail.com>
2026-04-06 18:48:55 +0530

    Median Of Two sorted Arrays

---
+++ blind75/src/MedianOfTwoSortedArrays_4.java
@@ -0,0 +1,68 @@
+public class MedianOfTwoSortedArrays_4 {
+
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        int low = 0; // Since there could be 0 elms from the left
+        int high = n1; // Since there could be all the elements from the left
+        int left = (n1 + n2 + 1) / 2;
+        int n = n1 + n2;
+
+        if(n1 > n2) return findMedianSortedArrays(nums2, nums1); // Choose the smaller array for better time complexity
+        while (low <= high) {
+            int mid1 = (low + high) / 2;
+            int mid2 =  left - mid1;
+
+            int l1 = Integer.MIN_VALUE, l2 = Integer.MIN_VALUE;
+            int r1 = Integer.MAX_VALUE, r2 = Integer.MAX_VALUE;
+
+            if(mid1 < n1) r1 = nums1[mid1];
+            if(mid2 < n2) r2 = nums2[mid2];
+            if(mid1 - 1 >= 0) l1 = nums1[mid1 - 1];
+            if(mid2 - 1 >= 0) l2 = nums2[mid2 - 1];
+
+            if(l1 <= r2 && l2 <= r1){
+                if(n % 2 == 1) return Math.max(l1, l2);
+                return (double) (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
+            }else if(l1 > r2) high = mid1 -1;
+            else low = mid1 + 1;
+        }
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[] arr1= {1};
+        int[] arr2 = {};
+        System.out.println(findMedianSortedArrays(arr1, arr2));
+    }
+}
+
+/*
+Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+The overall run time complexity should be O(log (m+n)).
+
+
+
+Example 1:
+
+Input: nums1 = [1,3], nums2 = [2]
+Output: 2.00000
+Explanation: merged array = [1,2,3] and median is 2.
+Example 2:
+
+Input: nums1 = [1,2], nums2 = [3,4]
+Output: 2.50000
+Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+
+
+Constraints:
+
+nums1.length == m
+nums2.length == n
+0 <= m <= 1000
+0 <= n <= 1000
+1 <= m + n <= 2000
+-106 <= nums1[i], nums2[i] <= 106
+ */
